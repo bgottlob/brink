5eeee291b61d874a74b359f05145c0aff5706533
Brandon Gottlob <info@bgottlob.com>
2019-07-27 23:44:10 -0400

    Useful unit tests

--- brink/test/brink_consumer_test.exs
+++ brink/test/brink_consumer_test.exs
@@ -4,6 +4,8 @@ defmodule BrinkTest do
   import Mock
   doctest Brink.Consumer
 
+  import Brink.Consumer
+
   test "Builds a spec in single mode" do
     {module, keywords} = Brink.Consumer.build_spec_single_mode("redis://hi", "streamname", [opt1: "hi"])
     assert module == Brink.Consumer
@@ -24,19 +26,39 @@ defmodule BrinkTest do
     end
   end
 
+  defp msgs(num) do
+    List.duplicate(["#{num}", ["thekey", "#{num}"]], num)
+  end
+
   test "handle_demand buffers demand when there is nothing to send back" do
-    with_mock Brink.Lib, [:passthrough], [xread: fn(_) -> {:ok, [["123-1", ["hey", "joe"]]]} end] do
-      assert {:noreply, [{"123-1", %{hey: "joe"}}], %{ demand: 9, mode: :single, next_id: "123-1", poll_interval: 100 }} == Brink.Consumer.handle_demand(5, %{mode: :single, demand: 5, next_id: "$", poll_interval: 100})
+    with_mock Brink.Lib, [:passthrough], [xread: fn(_) -> {:ok, msgs(0)} end] do
+      opts = %{mode: :single, next_id: "$", poll_interval: 100}
+      {:noreply, _, %{demand: 45}} = handle_demand(25, Map.put(opts, :demand, 20))
+      {:noreply, _, %{demand: 9}}  = handle_demand(4,  Map.put(opts, :demand,  5))
+      {:noreply, _, %{demand: 5}}  = handle_demand(0,  Map.put(opts, :demand,  5))
+      {:noreply, _, %{demand: 4}}  = handle_demand(4,  Map.put(opts, :demand,  0))
+      {:noreply, _, %{demand: 0}}  = handle_demand(0,  Map.put(opts, :demand,  0))
+      # TODO can demand be negative??
     end
   end
 
-  #property "handle_demand buffers demand when there is nothing to send back" do
-  #  forall {event} <- {event()} do
-  #    with_mock Brink.Lib, [:passthrough], [xread: fn(_) -> {:ok, [event]} end] do
-  #      assert {:noreply, [event], %{ demand: 9 }} == Brink.Consumer.handle_demand(5, %{mode: :single, demand: 5, next_id: "$", poll_interval: 100})
-  #    end
-  #  end
-  #end
+  test "buffer leftover demand when there aren't enough messages to satisfy it" do
+    with_mock Brink.Lib, [:passthrough], [xread: fn(_) -> {:ok, msgs(3)} end] do
+      opts = %{mode: :single, next_id: "$", poll_interval: 100}
+      {:noreply, _, %{demand: 2}} = handle_demand(5, Map.put(opts, :demand, 0))
+      {:noreply, _, %{demand: 1}}  = handle_demand(2,  Map.put(opts, :demand,  2))
+      {:noreply, _, %{demand: 15}}  = handle_demand(12,  Map.put(opts, :demand,  6))
+    end
+  end
 
-  def event(), do: [utf8(), [utf8(), utf8()]]
+  test "satisfy demand when there are enough messages" do
+    with_mock Brink.Lib, [:passthrough], [xread: fn(_) -> {:ok, msgs(10)} end] do
+      opts = %{mode: :single, next_id: "$", poll_interval: 100}
+      {:noreply, _, %{demand: 0}} = handle_demand(8, Map.put(opts, :demand, 2))
+      # TODO figure out if negative demand is a thing
+      #{:noreply, _, %{demand: 0}}  = handle_demand(1,  Map.put(opts, :demand,  2))
+      #{:noreply, _, %{demand: 0}}  = handle_demand(1,  Map.put(opts, :demand,  0))
+      {:noreply, _, %{demand: 0}}  = handle_demand(0,  Map.put(opts, :demand,  0))
+    end
+  end
 end
